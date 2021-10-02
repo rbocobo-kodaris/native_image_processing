e67af1937046859b551fe9a4c93c5cbdb94862df
Rhoderick Bocobo <rhoderick@kodaris.com>
2021-10-02 20:48:34 +0800

    getBattery implementation: Test method to call native android library

--- android/src/main/kotlin/com/kodaris/native_image_processing/NativeImageProcessingPlugin.kt
+++ android/src/main/kotlin/com/kodaris/native_image_processing/NativeImageProcessingPlugin.kt
@@ -1,5 +1,10 @@
 package com.kodaris.native_image_processing
 
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -15,8 +20,10 @@ class NativeImageProcessingPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var context : Context
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    context = flutterPluginBinding.getApplicationContext()
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com.kodaris/native_image_processing")
     channel.setMethodCallHandler(this)
   }
@@ -27,7 +34,8 @@ class NativeImageProcessingPlugin: FlutterPlugin, MethodCallHandler {
         result.success("Android ${android.os.Build.VERSION.RELEASE}")
       }
       "getBatteryLevel" -> {
-        result.success("100%");
+        val batterPercentage = getBatteryPercentage(context)
+        result.success("${batterPercentage}%");
       }
       else -> result.notImplemented()
     }
@@ -35,5 +43,20 @@ class NativeImageProcessingPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  fun getBatteryPercentage(context: Context): Int {
+    return if (Build.VERSION.SDK_INT >= 21) {
+      val bm = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+      bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
+    } else {
+      val iFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+      val batteryStatus = context.registerReceiver(null, iFilter)
+      val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+      val scale = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+      val batteryPct = level!! / scale?.toDouble()!!
+      (batteryPct * 100).toInt()
+    }
   }
 }
